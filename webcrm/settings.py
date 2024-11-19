7f330ba859faf190823a60410fd9ec9f346b0e5d
AdamMashaka <mashakaadam123@gmail.com>
2024-11-19 23:19:20 +0300

    new

--- webcrm/settings.py
+++ webcrm/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = 'j1c=6$s-dh#$ywt@(q4cm=j&0c*!0x!e-qm6k1%yoliec(15tn'
 # Add your hosts to the list.
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+
 # Database
 # Database
 DATABASES = {
